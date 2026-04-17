2924ba0f2a455fba0874df214ffcaf0bd3190cf3
Aman Setia <amansetiarjp@gmail.com>
2026-04-18 00:34:49 +0530

    Initial Commit

---
+++ wcsfm/wcsfmApp.swift
@@ -0,0 +1,17 @@
+//
+//  wcsfmApp.swift
+//  wcsfm
+//
+//  Created by Aman Setia on 18/04/26.
+//
+
+import SwiftUI
+
+@main
+struct wcsfmApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
